389733699da0596e6a0204ddc2f78fa986a3db57
Aurelien671 <36074358+Aurelien671@users.noreply.github.com>
2018-02-02 13:25:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+
+bot.on('message',function(message){
+    if (message.content ==='!helpMusique'){
+        message.reply('!!!play <link> pour commencer à jouer une chanson,!!!stop pour arrêter de jouer et partir.')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='Bonne nuit'){
+        message.reply('Dors-bien :)')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='salut ange671'){
+        message.reply('Salut')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='slt ange671'){
+        message.reply('Salut')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='hi ange671'){
+        message.reply('Hi')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='Hi Ange671'){
+        message.reply('Hi')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='slt Ange671'){
+        message.reply('Salut')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='Salut Ange671'){
+        message.reply('Salut')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='bonne nuit'){
+        message.reply('Dors-bien :)')
+    }
+})
+
+bot.on('message',function(message){
+    if (message.content ==='bonjours'){
+        message.reply('bonjour')
+    }
+})
+
+
+
+bot.login('NDA4NzAxODQyNDI0NTk0NDMz.DVT52A.RBfjvUQA8MxnNEHzhyQoi-0oCkQ')
